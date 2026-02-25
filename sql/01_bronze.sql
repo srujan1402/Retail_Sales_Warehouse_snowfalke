664a32c94a4f92163933407b8b4967ee8c2cc78f
Srujan reddy <mallesh.srujan.reddy@gmail.com>
2026-02-25 06:24:45 +0000

    add

---
+++ sql/01_bronze.sql
@@ -0,0 +1,35 @@
+use database snowflake_sample_data;
+
+desc table tpcds_sf100tcl.store_sales;
+
+select count(*) from tpcds_sf100tcl.store_sales;
+
+
+use warehouse compute_wh;
+
+use database snowflake_learning_db;
+
+create schema if not exists bronze;
+
+create or replace table bronze.store_sales_raw as 
+ select *
+  from snowflake_sample_data.tpcds_sf100tcl.store_sales
+  limit 500000000;
+
+  --phase 3: data validation
+  select count(*) from bronze.store_sales_raw;
+
+  select count(*)
+  from snowflake_sample_data.tpcds_sf100tcl.store_sales;
+
+
+  desc table bronze.store_sales_raw;
+
+
+  --basic null check
+  select count(*) as total_rows, count(ss_sales_price) as non_null_sales_price
+   from bronze.store_sales_raw;
+
+   --optional
+   select min(ss_sold_date_tsk) , max(ss_sold_date_sk)
+   from bronze.store_sales_raw;
